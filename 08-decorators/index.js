8879ed92064693981af1378f025e7762edb11c88
superphung <superphung44@gmail.com>
2015-09-20 04:18:49 +0200

    finished 8

--- 08-decorators/index.js
+++ 08-decorators/index.js
@@ -6,7 +6,9 @@ var app = module.exports = koa();
 
 app.use(function* (next) {
   yield next;
-  // add some logic here!
+    // add some logic here!
+    var res = escape(this.response.body);
+    this.response.body = res;
 })
 
 app.use(function* body() {
