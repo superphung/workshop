6994fe5b371215c3bb3eab4e5fa07fcb7d0df0f0
superphung <superphung44@gmail.com>
2015-09-20 03:35:10 +0200

    finished 7

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,10 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+    if (this.is('text/plain')) this.body = 'ok';
+    if(this.is('application/json')) {
+	this.body = {
+	    message: 'hi!'
+	};
+    }
 })
