e9a6a677b2a18ff17be136b6459be79bf9b1853e
superphung <superphung44@gmail.com>
2015-09-20 02:54:08 +0200

    finished 3

--- 03-routing/index.js
+++ 03-routing/index.js
@@ -3,14 +3,16 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
+app.use(function* (next) {
+    if (this.request.url === '/') this.body = 'hello world';
+    yield next;
+});
 
+app.use(function* (next) {
+    if (this.request.url === '/404') this.body = 'page not found';
+    yield next;
 });
 
 app.use(function* () {
-
-});
-
-app.use(function* () {
-
+    if (this.request.url === '/500') this.body = 'internal server error';
 });
